55fd6ad7caca415632ec27a458e355e65605de29
Kinebuchi Tomohiko <kinebuchi@nautilus-technologies.com>
2016-06-02 11:45:21 +0900

    Fill configuration for setup.py

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@
  limitations under the License.
 """
 
-from setuptools import setup
+from setuptools import setup, find_packages
 import os
 
 version='0.1.0'
@@ -24,9 +24,36 @@ long_description = '\n'.join([
     open(os.path.join('.', 'README.rst')).read(),
 ])
 
+classifiers = [
+    'Development Status :: 1 - Planning',
+    'License :: OSI Approved :: Apache Software License',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 3.5',
+    'Topic :: Documentation',
+]
+
 setup(
     name='pygments-dmdl',
     version=version,
     description='DMDL lexer and highlighter for Pygments',
     long_description=long_description,
+    classifiers=classifiers,
+    keywords=[],
+    author='cocoatomo',
+    author_email='cocoatomo77 at gmail dot com',
+    url='https://github.org/cocoatomo/pygments-dmdl',
+    license='Apache License (2.0)',
+    namespace_packages=['dmdl'],
+    packages=find_packages('.'),
+    package_dir={'': '.'},
+    # package_data = {'': ['buildout.cfg']},
+    include_package_data=True,
+    install_requires=[
+        'setuptools',
+    ],
+    entry_points="""
+    """,
+    zip_safe=False,
+    # tests_require=['pytest'],
+    # cmdclass={'test': PyTest},
 )
